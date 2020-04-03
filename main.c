6c20c509a67e3ba6c6dc6c16aa53605b9806227c
Antoine Le Coustumer <antoine.lecoustumer@hotmail.fr>
2020-04-03 10:58:37 +0200

    include bibliotheques +debut main

--- main.c
+++ main.c
@@ -1,3 +1,15 @@
-int main.c {
+#include <stdio.h>
+#include <stdlib.h>
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_primitives.h>
+#include <time.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>
 
-};
+int main() {
+    ALLEGRO_DISPLAY *display = NULL;
+    ALLEGRO_EVENT_QUEUE *queue = NULL;
+    ALLEGRO_TIMER *timer = NULL;
+    ALLEGRO_EVENT event = {0};
+}
